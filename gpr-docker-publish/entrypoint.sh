0a1342a5cc7e1b1fa549dfca9044ccf43f6eee4f
Hamel Husain <hamel.husain@gmail.com>
2019-09-04 20:22:04 -0700

    switched name

--- gpr-docker-publish/entrypoint.sh
+++ gpr-docker-publish/entrypoint.sh
@@ -8,12 +8,11 @@
 # send credentials through stdin (it is more secure)
 echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin docker.pkg.github.com
 
-
 # Set Local Variables
-shortSha=$(echo "${GITHUB_SHA}" | cut -c1-6)
+shortSHA=$(echo "${GITHUB_SHA}" | cut -c1-6)
 timestamp=`date +%Y%m%d%H%M%S`
 BASE_NAME="docker.pkg.github.com/${GITHUB_REPOSITORY}/${INPUT_IMAGE_NAME}"
-SHA_NAME="${BASE_NAME}:${timestamp}${shortSha}"
+SHA_NAME="${BASE_NAME}:${timestamp}${shortSHA}"
 
 # Build The Container
 docker build -t ${SHA_NAME} -t ${BASE_NAME} -f ${INPUT_DOCKERFILE_PATH} ${INPUT_BUILD_CONTEXT}
@@ -22,5 +21,5 @@ docker build -t ${SHA_NAME} -t ${BASE_NAME} -f ${INPUT_DOCKERFILE_PATH} ${INPUT_
 docker push ${BASE_NAME}
 docker push ${SHA_NAME}
 
-echo "::set-output name=IMAGE_SHA_NAME::${BASE_NAME}"
+echo "::set-output name=IMAGE_SHA_NAME::${SHA_NAME}"
 echo "::set-output name=IMAGE_URL::https://github.com/${GITHUB_REPOSITORY}/packages"
