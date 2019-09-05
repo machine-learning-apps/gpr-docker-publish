23cfecb6552ca5fb6af83a1c4829dce8e1e0ceb5
Hamel Husain <hamel.husain@gmail.com>
2019-09-04 19:31:57 -0700

    test

--- gpr-docker-publish/entrypoint.sh
+++ gpr-docker-publish/entrypoint.sh
@@ -6,24 +6,21 @@
 # The following environment variables will be provided by the environment automatically: GITHUB_REPOSITORY, GITHUB_SHA
 
 # send credentials through stdin (it is more secure)
-
-echo "Hello World"
-
-# echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin docker.pkg.github.com
+echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin docker.pkg.github.com
 
 
-# # Set Local Variables
-# shortSha=$(echo "${GITHUB_SHA}" | cut -c1-6)
-# timestamp=`date +%Y%m%d%H%M%S`
-# BASE_NAME="docker.pkg.github.com/${GITHUB_REPOSITORY}/${INPUT_IMAGE_NAME}"
-# SHA_NAME="${BASE_NAME}:${timestamp}${shortSha}"
+# Set Local Variables
+shortSha=$(echo "${GITHUB_SHA}" | cut -c1-6)
+timestamp=`date +%Y%m%d%H%M%S`
+BASE_NAME="docker.pkg.github.com/${GITHUB_REPOSITORY}/${INPUT_IMAGE_NAME}"
+SHA_NAME="${BASE_NAME}:${timestamp}${shortSha}"
 
-# # Build The Container
-# docker build -t ${SHA_NAME} -t ${BASE_NAME} -f ${INPUT_DOCKERFILE_PATH} ${INPUT_BUILD_CONTEXT}
+# Build The Container
+docker build -t ${SHA_NAME} -t ${BASE_NAME} -f ${INPUT_DOCKERFILE_PATH} ${INPUT_BUILD_CONTEXT}
 
-# # Push two versions, with and without the SHA
-# docker push ${BASE_NAME}
-# docker push ${SHA_NAME}
+# Push two versions, with and without the SHA
+docker push ${BASE_NAME}
+docker push ${SHA_NAME}
 
-# echo "::set-output name=IMAGE_SHA_NAME::${BASE_NAME}"
-# echo "::set-output name=IMAGE_URL::https://github.com/${GITHUB_REPOSITORY}/packages"
+echo "::set-output name=IMAGE_SHA_NAME::${BASE_NAME}"
+echo "::set-output name=IMAGE_URL::https://github.com/${GITHUB_REPOSITORY}/packages"
